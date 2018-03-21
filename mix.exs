4ed1d888413fcab8e304a01a375819c5d5a360ab
Lukas Rieder <l.rieder@gmail.com>
2018-03-21 16:01:28 +0100

    put statix in extra_applications

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Vitalstatistix.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :statix]
     ]
   end
 
